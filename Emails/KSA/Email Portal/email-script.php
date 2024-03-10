9443918fdc6a3fde732dfb6c09f2b3ed99e3bb09
Waqar Azeem <56077768+WAQAR-54@users.noreply.github.com>
2024-03-10 18:54:15 +0500

    Email Flyer

--- Emails/KSA/Email Portal/email-script.php
+++ Emails/KSA/Email Portal/email-script.php
@@ -1,35 +1,43 @@
 <?php
-if (isset($_POST['sendMailBtn'])) {
-    $fromEmail = $_POST['fromEmail'];
-    $toEmail = $_POST['toEmail'];
-    $subjectName = $_POST['subject'];
-    $message = $_POST['message'];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    $to = $toEmail;
-    $subject = $subjectName;
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= 'From: '.$fromEmail.'<'.$fromEmail.'>' . "\r\n".'Reply-To: '.$fromEmail."\r\n" . 'X-Mailer: PHP/' . phpversion();
-    $message = '<!doctype html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<meta name="viewport"
-					  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-				<meta http-equiv="X-UA-Compatible" content="ie=edge">
-				<title>Document</title>
-			</head>n
-			<body>
-			<span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">'.$message.'</span>
-				<div class="container">
-                 '.$message.'<br/>
-                    Regards<br/>
-                  '.$fromEmail.'
-				</div>
-			</body>
-			</html>';
-    $result = @mail($to, $subject, $message, $headers);
+require 'vendor/autoload.php';
 
-    echo '<script>alert("Email sent successfully !")</script>';
-    echo '<script>window.location.href="https://fivestar.pk/test/index.php";</script>';
+// If you're not using Composer, include the necessary PHPMailer files manually
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $senderEmail = $_POST['senderEmail'];
+    $recipientEmail = $_POST['recipientEmail'];
+    $subject = $_POST['subject'];
+    $messageBody = $_POST['messageBody'];
+
+    // Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                        // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'waqarazeem54003@gmail.com';          // SMTP username
+        $mail->Password   = 'Zawia@001@001';                  // SMTP password
+        $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_STARTTLS` also accepted
+        $mail->Port       = 587;                                    // TCP port to connect to
+
+        //Recipients
+        $mail->setFrom($senderEmail);
+        $mail->addAddress($recipientEmail);                         // Add a recipient
+
+        // Content
+        $mail->isHTML(true);                                       // Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body    = $messageBody;
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
+?>
